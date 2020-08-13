98d2b17ec48fa6900a2431740841b3f6dbfb63ae
David Mar <dave.mar@gmail.com>
2020-08-13 12:26:55 -0700

    Completed CSS for all components. Some areas need some more refinement.

--- client/src/Findmore.jsx
+++ client/src/Findmore.jsx
@@ -7,26 +7,22 @@ var Findmore = () => {
     <div className="findmorecomponentdv">
       <h3 className="findmoretitledv">Find More on Reverb</h3>
 
-      <div className="findmoreproductdv">
-
-        <img className="findmoreimagedv" src="https://images.reverb.com/image/upload/s--rz5ClWDv--/t_card-square/v1591382671/eynovcjrjsddmvskgdya.jpg" />
-        <div>
-          <h3>Squier Paranormal Super-Sonic</h3>
-          <StarRatingComponent name="rate1" starCount={5} value={5} />
-          <div>(2)</div>
+      <div className="findmorecontainerdiv">
+        <div className="findmoreimagedivdv">
+          <img className="findmoreimagedv" src="https://images.reverb.com/image/upload/s--2v8soTt2--/f_auto,t_large/v1597242733/u8q7dxguafrlyinnaboo.jpg" />
         </div>
-
+        <div>
+          <div>
+            <h3 className="findmorenamedv">Squier Paranormal Super-Sonic</h3>
+          </div>
+          <div className="sellerreviewcountdv"><StarRatingComponent name="rate1" starCount={5} value={5} emptyStarColor={"#FFF0DA"} />(3,948)
+          </div>
+        </div>
       </div>
-
-
-
-
-
-
-        <a className="csp-pg-card__price-link" href="https://reverb.com/p/squier-paranormal-super-sonic/new">59 new from $349.99</a>
-        <a className="csp-pg-card__price-link" href="https://reverb.com/p/squier-paranormal-super-sonic/  used">3 used from $299.99</a>
-
-
+      <div className="findmorelinksdivdv">
+        <a className="findmorelinksdv" href="https://reverb.com/p/squier-paranormal-super-sonic/new">59 new from $349.99</a>
+        <a className="findmorelinksdv" href="https://reverb.com/p/squier-paranormal-super-sonic/  used">3 used from $299.99</a>
+      </div>
 
 
     </div>
