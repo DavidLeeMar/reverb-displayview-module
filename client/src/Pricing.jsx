dad83b0d0cf46a2450a1566aea003f7c4eb3f72a
David Mar <dave.mar@gmail.com>
2020-08-19 22:19:00 -0700

    minor css changes

--- client/src/Pricing.jsx
+++ client/src/Pricing.jsx
@@ -9,7 +9,7 @@ var Pricing = (props) => {
       <button type="submit" className="addtocartbuttondv">Add to Cart</button>
       <button title="Watch" className="watchbuttondv" onClick={props.handleSignUpModalToggle}>
       <div className="pricewatchdivdv"><i className="far fa-star"></i><span>Watch</span></div></button>
-      <div className="dropalertsdivdv"><img className="dropalertpngdv" src="./dropalerts.png"></img></div>
+      <div className="dropalertsdivdv"><img className="dropalertpngdv" src="https://dropimage.s3-us-west-1.amazonaws.com/dropalerts.png"></img></div>
     </div>
   )
 }
