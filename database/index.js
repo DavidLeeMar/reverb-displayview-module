b5b161e9da6a3b8083ddc0b867af4700a161f0bc
Miksemaj <jskim983@gmail.com>
2020-08-19 22:28:21 -0700

    seeded DB

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'student',
+  password : 'jameskim',
   database : 'reverb_display'
 });
 
